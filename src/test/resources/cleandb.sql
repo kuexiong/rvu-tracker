95f4a32213b9b5de664d9be24bc650c7614e966c
= <kuexiong14@gmail.com>
2023-02-10 23:28:27 -0600

    Fix table name and add a user for testing

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,2 +1,2 @@
-delete from user;
-INSERT INTO user VALUES (1,'Joe','Coyne','jcoyne','supersecret1','1964-04-01'),(2,'Fred','Hensen','fhensen','supersecret2','1988-05-08'),(3,'Barney','Curry','bcurry','supersecret3','1947-11-11'),(4,'Karen','Mack','kmack','supersecret4','1986-07-08'),(5,'Dianne','Klein','dklein','supersecret5','1991-09-22'),(6,'Dawn','Tillman','dtillman','supersecret6','1979-08-30');
+delete from users;
+INSERT INTO users VALUES (1,'Kue','Xiong','kxiong1@madisoncollege.edu','hello');
