cf41ba7efeb465b82cc95a17164f18e6fb6bf796
= <kuexiong14@gmail.com>
2023-02-11 21:14:09 -0600

    Add a second user to database

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,2 +1,2 @@
 delete from users;
-INSERT INTO users VALUES (1,'Kue','Xiong','kxiong1@madisoncollege.edu','hello');
+INSERT INTO users VALUES (1,'Kue','Xiong','kxiong1@madisoncollege.edu','hello'),(2, 'Spongebob', 'Square Pants', 'sponge@underthesea.com', 'gary');
