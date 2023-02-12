1602ee1415d1c51e92e8374ea323a59ba28089bd
= <kuexiong14@gmail.com>
2023-02-11 21:51:20 -0600

    Initialize sign-up servlet

---
+++ src/main/java/com/rvutracker/controller/SignUp.java
@@ -0,0 +1,65 @@
+package com.rvutracker.controller;
+
+import com.rvutracker.entity.User;
+import com.rvutracker.persistence.UserDao;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * A servlet to sign up and create a user account.
+ *
+ * @author Kue Xiong
+ */
+
+@WebServlet(
+        urlPatterns = {"/signUpServlet"}
+)
+public class SignUp extends HttpServlet {
+    /**
+     * Handles HTTP GET requests
+     *
+     * @param request Description of the Parameter
+     * @param response Description of the Parameter
+     * @throws ServletException if there is a Servlet failure
+     * @throws IOException if there is an IO failure
+     */
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Instantiate UserDao object.
+        UserDao addNewUser = new UserDao();
+
+        // Get user information from HTML form
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        // Instantiate new user.
+        User user = new User(firstName, lastName, email, password);
+
+        //Run method to insert into database.
+        addNewUser.insert(user);
+
+        // Store success message in session to display on JSP
+        // when user account has been created.
+        HttpSession session = request.getSession();
+
+        String sessionMessage = "Account created. Return to login.";
+        session.setAttribute("accountCreatedMessage", sessionMessage);
+
+        //TODO: need sign-in servlet
+
+        // Redirect browser back to sign-in
+        String url = "/signInServlet";
+        response.sendRedirect(request.getContextPath() + url);
+    }
+}
