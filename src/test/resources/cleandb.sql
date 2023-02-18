4f06fceb5ff5e4f54d72ddc5b41a7a6156766e0d
= <kuexiong14@gmail.com>
2023-02-17 21:02:49 -0600

    Add insert statement to patient table

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,2 +1,9 @@
-delete from users;
-INSERT INTO users VALUES (1,'Kue','Xiong','kxiong1@madisoncollege.edu','hello'),(2, 'Spongebob', 'Square Pants', 'sponge@underthesea.com', 'gary');
+delete from user;
+INSERT INTO user VALUES (1,'Kue','Xiong','kxiong1@madisoncollege.edu','hello'),
+                        (2, 'Spongebob', 'Square Pants', 'sponge@underthesea.com', 'gary');
+
+delete from patient;
+INSERT INTO patient VALUES (1,'Peppa','Pig','2023-01-12','Anxiety'),
+                           (2,'George','Pig','2022-12-05','ADHD'),
+                           (3,'Grampy','Rabbit','2023-01-08','Memory complaints'),
+                           (1,'Zuzu','Zebra','2023-02-10','TBI');
