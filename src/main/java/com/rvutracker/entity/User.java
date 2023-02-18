fb294cb8672710c6f88f37bd7f435a0f33be4efc
= <kuexiong14@gmail.com>
2023-02-17 19:24:12 -0600

    Initialize equals() and hashCode()

--- src/main/java/com/rvutracker/entity/User.java
+++ src/main/java/com/rvutracker/entity/User.java
@@ -148,11 +148,23 @@ public class User {
         this.password = password;
     }
 
-    /**
-     * String outputting all of the user's information.
-     *
-     * @return
-     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, email, password, id);
+    }
+
     @Override
     public String toString() {
         return "User{" +
