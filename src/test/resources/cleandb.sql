bdb724c1df7a32eaa9fbe9d395c1dabe23065a79
= <kuexiong14@gmail.com>
2023-02-17 23:24:39 -0600

    Refactor delete from patient statement

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,9 +1,8 @@
 delete from user;
+delete from patient;
 INSERT INTO user VALUES (1,'Kue','Xiong','kxiong1@madisoncollege.edu','hello'),
                         (2, 'Spongebob', 'Square Pants', 'sponge@underthesea.com', 'gary');
-
-delete from patient;
-INSERT INTO patient VALUES (1,'Peppa','Pig','2023-01-12','Anxiety'),
-                           (2,'George','Pig','2022-12-05','ADHD'),
-                           (3,'Grampy','Rabbit','2023-01-08','Memory complaints'),
-                           (1,'Zuzu','Zebra','2023-02-10','TBI');
+INSERT INTO `patient` VALUES (1,'Peppa','Pig','2023-01-12','Anxiety',1),
+                           (2,'George','Pig','2022-12-05','ADHD',1),
+                           (3,'Grampy','Rabbit','2023-01-08','Memory complaints',2),
+                           (4,'Zuzu','Zebra','2023-02-10','TBI',1);
