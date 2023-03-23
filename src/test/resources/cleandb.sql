9bf382f40f846393dadc42e632f89f26e6d51fa4
= <kuexiong14@gmail.com>
2023-03-23 00:49:42 -0500

    Remove password column
    - Password will be stored on Cognito

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,7 +1,7 @@
 delete from user;
 delete from patient;
-INSERT INTO user VALUES (1,'Kue','Xiong','kxiong1@madisoncollege.edu','hello'),
-                        (2, 'Spongebob', 'Square Pants', 'sponge@underthesea.com', 'gary');
+INSERT INTO user VALUES (1,'Kue','Xiong','kxiong1@madisoncollege.edu','kxiong1'),
+                        (2, 'Spongebob', 'Square Pants', 'sponge@underthesea.com','sponge');
 INSERT INTO `patient` VALUES (1,'Peppa','Pig','2023-01-12','Anxiety','In Progress',1),
                            (2,'George','Pig','2022-12-05','ADHD','In Progress',1),
                            (3,'Grampy','Rabbit','2023-01-08','Memory complaints','Signed',2),
