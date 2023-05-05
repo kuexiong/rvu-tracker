165f3122a03c3e7e3783eb46ecddbd4d659a97cd
kuexiong <kuexiong14@gmail.com>
2023-05-04 22:19:43 -0500

    Update test values

--- src/test/java/com/rvutracker/persistence/CalculateRVUTest.java
+++ src/test/java/com/rvutracker/persistence/CalculateRVUTest.java
@@ -1,5 +1,7 @@
 package com.rvutracker.persistence;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
 import java.time.Month;
@@ -11,25 +13,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculateRVUTest {
 
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
     @Test
     void getCurrentMonth() {
         CalculateRVU getMonth = new CalculateRVU();
         Month month = getMonth.getCurrentMonth();
         String monthName = month.getDisplayName(TextStyle.FULL_STANDALONE, Locale.ENGLISH);
-        assertEquals("April", monthName);
+        assertEquals("May", monthName);
     }
 
-    // TODO: assertion
     @Test
-    void calculateFiscalYear() {
-        CalculateRVU test = new CalculateRVU();
-        test.calculateFiscalYear();
+    void getCurrentYear() {
+        CalculateRVU getYear = new CalculateRVU();
+        int currentYear = getYear.getCurrentYear();
+        assertEquals(2023, currentYear);
     }
+
+//    // TODO: assertion
+//    @Test
+//    void calculateFiscalYear() {
+//        CalculateRVU test = new CalculateRVU();
+//        test.calculateFiscalYear();
+//    }
 
     // TODO: assertion
     @Test
     void calculateSuccess() {
         CalculateRVU test = new CalculateRVU();
-        test.calculate();
+        test.calculate(1);
     }
 }
