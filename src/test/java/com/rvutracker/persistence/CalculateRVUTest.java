6e52e3f9da3d8fac8e0a568e095a3155535075a7
kuexiong <kuexiong14@gmail.com>
2023-05-07 20:37:13 -0500

    Add additional tests

--- src/test/java/com/rvutracker/persistence/CalculateRVUTest.java
+++ src/test/java/com/rvutracker/persistence/CalculateRVUTest.java
@@ -11,10 +11,16 @@ import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * The type Calculate rvu test.
+ */
 class CalculateRVUTest {
 
     private final Logger logger = LogManager.getLogger(this.getClass());
 
+    /**
+     * Gets current month success.
+     */
     @Test
     void getCurrentMonth() {
         CalculateRVU getMonth = new CalculateRVU();
@@ -23,6 +29,9 @@ class CalculateRVUTest {
         assertEquals("May", monthName);
     }
 
+    /**
+     * Gets current year success.
+     */
     @Test
     void getCurrentYear() {
         CalculateRVU getYear = new CalculateRVU();
@@ -30,17 +39,27 @@ class CalculateRVUTest {
         assertEquals(2023, currentYear);
     }
 
-//    // TODO: assertion
-//    @Test
-//    void calculateFiscalYear() {
-//        CalculateRVU test = new CalculateRVU();
-//        test.calculateFiscalYear();
-//    }
-
-    // TODO: assertion
+    /**
+     * Calculate fiscal start success.
+     */
     @Test
-    void calculateSuccess() {
-        CalculateRVU test = new CalculateRVU();
-        test.calculate(1);
+    void calculateFiscalStart() {
+        CalculateRVU getStartYear = new CalculateRVU();
+        Calendar fiscalStartYear = getStartYear.calculateFiscalStart();
+        int year = (fiscalStartYear.get(Calendar.YEAR));
+        assertEquals(2022, year);
     }
+
+    /**
+     * Calculate fiscal end success.
+     */
+    @Test
+    void calculateFiscalEnd() {
+        CalculateRVU getEndYear = new CalculateRVU();
+        Calendar fiscalEndYear = getEndYear.calculateFiscalEnd();
+        int year = (fiscalEndYear.get(Calendar.YEAR));
+        assertEquals(2023, year);
+    }
+
+
 }
